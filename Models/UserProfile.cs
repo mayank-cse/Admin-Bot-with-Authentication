72dc68248a53e7cf08b6d154ca323dc48715cc74
mayank-cse <72187020+mayank-cse@users.noreply.github.com>
2022-07-16 21:32:39 +0530

    code

---
+++ Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceAdminBot.Models
+{
+    public class UserProfile
+    {
+        public bool UserAuthenticated { get; set; } = false;
+        public int OTP { get; set; }
+        public string ValueFinder { get; set; }
+    }
+}
